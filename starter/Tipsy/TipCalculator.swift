d825e8d2b9f1c52e9a73e4ea9d06b53d0e7fd993
Alex Brown <abrown252@gmail.com>
2020-07-11 16:05:28 +0100

    Finished starter and final projects

--- starter/Tipsy/TipCalculator.swift
+++ starter/Tipsy/TipCalculator.swift
@@ -1,11 +1,138 @@
-//
-//  TipCalculator.swift
-//  TipCalculator
-//
-//  Created by Alex Brown on 02/07/2020.
-//  Copyright © 2020 ABrown. All rights reserved.
-//
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import Foundation
+import UIKit
 
-struct TipCalculator {}
+class TipCalculator {
+  private(set) var billAmount = 1000.0
+  private(set) var tipPercentage = 15.0
+  private let allowedCharacterSet = "0123456789.,"
+  private let imageReactionNames = ["unamused", "worried", "happy", "smile", "surprised"]
+
+  private var tipAmount: Double {
+    return (billAmount / 100) * tipPercentage
+  }
+
+  private var billTotal: Double {
+    return billAmount + tipAmount
+  }
+
+  var billAmountCurrencyString: String {
+    return "\(billAmount.currencyFormatted() ?? "0")"
+  }
+
+  var billAmountString: String {
+    return "\(billAmount)"
+  }
+
+  var tipPercentageString: String {
+    return String(format: "%.0f", tipPercentage)
+  }
+
+  var tipAmountString: String {
+    return "\(tipAmount.currencyFormatted() ?? "0")"
+  }
+
+  var billTotalString: String {
+    return "\(billTotal.currencyFormatted() ?? "0")"
+  }
+
+  //swiftlint:disable force_unwrapping
+  var reactionForTipPercentage: UIImage {
+    switch tipPercentage {
+    case (0...9):
+      return UIImage(named: imageReactionNames[0])!
+    case (10...19):
+      return UIImage(named: imageReactionNames[1])!
+    case (20...29):
+      return UIImage(named: imageReactionNames[2])!
+    case (30...59):
+      return UIImage(named: imageReactionNames[3])!
+    default:
+      return UIImage(named: imageReactionNames[4])!
+    }
+  }
+  //swiftlint:enable force_unwrapping
+
+  /**
+    Sets the `billAmount` from a String value, it supports currency strings, strings with separators and plain number String. For example:
+    ```
+    // The following String formats are supported
+    "10000.0" // Numbers
+    "10,000"  // Separator
+    "£10,000" // Currency
+    ```
+    - parameter totalString: The string representation of the bill amount, usually coming from a user input via `UITextField`
+  */
+  func setBillAmount(_ totalString: String ) {
+    if totalString.isEmpty {
+      billAmount = 0
+      return
+    }
+
+    if let total = Double(totalString) {
+      billAmount = total
+    } else if let total = totalString.numberFromCurrencyFormatting()?.doubleValue {
+      billAmount = total
+    } else if let total = totalString.numberFromSeparatorFormatting()?.doubleValue {
+      billAmount = total
+    }
+  }
+
+  func setTip(_ totalString: String) {
+    if totalString.isEmpty {
+      setTip(0)
+      return
+    }
+
+    guard let tip = Double(totalString)
+      else { return }
+
+    if tip > 100 {
+      setTip(100)
+    } else {
+      setTip(tip)
+    }
+  }
+
+  func setTip(_ tip: Double) {
+    tipPercentage = tip
+  }
+
+  /// Checks to see if the provided `String` is a double
+  /// - parameter str: The `String` value to check against
+  /// - returns: `true` if `str` is in the allowed set of characters
+  func isCharacterAllowed(_ str: String) -> Bool {
+    return Double(str) != nil || str.isEmpty
+  }
+}
--- starter/Tipsy/TipCalculatorViewController.swift
+++ starter/Tipsy/TipCalculatorViewController.swift
@@ -1,20 +1,78 @@
-//
-//  ViewController.swift
-//  TipCalculator
-//
-//  Created by Alex Brown on 02/07/2020.
-//  Copyright © 2020 ABrown. All rights reserved.
-//
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import UIKit
 
 class TipCalculatorViewController: UITableViewController {
+  var tipCalculator = TipCalculator()
+  @IBOutlet weak var keyboardAccessory: UIView!
+
   // MARK: View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Tip Calculator"
   }
-}
 
-// MARK: UITextFieldDelegate
-extension TipCalculatorViewController: UITextFieldDelegate {}
+  func configureTextFields() {
+    // TODO: Text field configuration
+  }
+
+  func updateTotals() {
+    // TODO: Update Total labels
+  }
+
+  func updateTipPercentage() {
+    // TODO: Update Tipsy image and tip percentage
+  }
+
+  /// Updates the UIStepper with the latest value from tipCalculator
+  func updateTipStepperValue() {
+    // TODO: Update stepper value when updated from elsewhere
+  }
+
+  func updateBillAmount() {
+    // TODO: Update billTextField
+  }
+
+  // MARK: IBOutlets
+  @IBAction func doneEditing() {
+    updateBillAmount()
+    updateTotals()
+    updateTipPercentage()
+  }
+
+  @IBAction func tipPercentageStepperValueChanged(_ sender: UIStepper) {
+    tipCalculator.setTip(sender.value)
+    updateTipPercentage()
+    updateTotals()
+  }
+}
